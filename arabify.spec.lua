b54c60acca57dec29185db3ea612bf4635019bd1
lispy-dobby <yilkalargawworkneh@gmail.com>
2018-10-18 22:59:22 +0300

    Create arabify.spec.lua

---
+++ arabify.spec.lua
@@ -0,0 +1,11 @@
+local geezify = require('arabiify-lua')
+local test_data = require('test_case')
+
+describe('arabify', function()
+	    it('should convert every geez number in the test data to its equivalent arab number', function()
+		  for i,v in ipairs(test_data) do
+		     print(v[2])
+		     assert.equal(geezify.geezify(v[2]), v[1])
+		  end
+  end)
+end)
