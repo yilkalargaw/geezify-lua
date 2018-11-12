77ad13bb757480e939fe10a1c2258b39ecdd194d
lispy-dobby <yilkalargawworkneh@gmail.com>
2018-11-12 07:13:05 +0300

    modified and linted arabify.lua using luacheck

--- geezify.lua
+++ geezify.lua
@@ -1,26 +1,24 @@
-geezify={}
+local geezify={}
 
 function geezify.geezify_2digit(num)
    local oneth_array = {'', '፩', '፪', '፫', '፬', '፭', '፮', '፯', '፰', '፱'}
    local tenth_array = {'', '፲', '፳', '፴', '፵', '፶', '፷', '፸', '፹', '፺'}
 
-   tenth_index = math.floor(num / 10)
-   oneth_index = num % 10
+   local tenth_index = math.floor(num / 10)
+   local oneth_index = num % 10
 
    return tenth_array[tenth_index+1] .. oneth_array[oneth_index+1]
 end
 
 function geezify.geezify_4digit(num)
-   first2 = math.floor(num/100)
-   second2 = num%100
+   local first2 = math.floor(num/100)
+   local second2 = num%100
 
    if first2==0 then
       return geezify.geezify_2digit(second2)
    else
       return geezify.geezify_2digit(first2) ..'፻'.. geezify.geezify_2digit(second2)
    end
-   
-   
 end
 
 function geezify.split_every_4_digit(num)
@@ -28,7 +26,7 @@ function geezify.split_every_4_digit(num)
    local a={}
 
    table.insert(a, string.sub(num,1, string.len(num)%4))
-   
+
    for digits in string.gmatch(string.sub(num,(string.len(num)%4)+1 ,-1) ,"%d%d%d%d") do
       table.insert(a , digits)
    end
@@ -47,7 +45,7 @@ function geezify.geezify(num)
 	 converted = converted
       else
 	 if converted==nil or converted == '' then
-	    converted = (conveted or '') .. geezify.geezify_4digit(v)
+	    converted = (converted or '') .. geezify.geezify_4digit(v)
 	 else
 	    converted = (converted or "") ..'፼'.. geezify.geezify_4digit(v)
 	 end
