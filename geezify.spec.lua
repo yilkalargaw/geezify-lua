06085390d53b11be00d24c87c5698f9a15af40eb
yilkalargaw <yilkalargawworkneh@gmail.com>
2019-07-25 03:17:23 +0300

    added the global geezifylua module for namespacing

--- geezify.spec.lua
+++ geezify.spec.lua
@@ -1,11 +1,11 @@
-local geezify = require('geezify')
+local geezifylua = require('geezifylua')
 local test_data = require('test_case')
 
 describe('geezify', function()
 	    it('should convert every number in the test data to its equivalent geez number', function()
 		  for i,v in ipairs(test_data) do
 		     print(v[1])
-		     assert.equal(geezify.geezify(v[1]), v[2])
+		     assert.equal(geezifylua.geezify.geezify(v[1]), v[2])
 		  end
   end)
 end)
